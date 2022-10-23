3e1b069988e74a298d11ea02f6bfc0258ee10f84
Emiri-Kakazu <emimail666@hotmail.co.jp>
2022-10-23 17:54:05 +0900

    fix_sell_items_3

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :purchase
+  #has_one :purchase
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -29,6 +29,5 @@ class Item < ApplicationRecord
     validates :days_to_ship_id
   end
 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: {only_integer: true}, { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
