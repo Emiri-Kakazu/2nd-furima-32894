8acbbe46832c4108f27d9652ec1d6268b0cb320c
Emiri-Kakazu <76907489+Emiri-Kakazu@users.noreply.github.com>
2022-10-23 19:21:27 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,5 +29,5 @@ class Item < ApplicationRecord
     validates :days_to_ship_id
   end
 
-  validates :price, presence: true, numericality: {only_integer: true}, { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
