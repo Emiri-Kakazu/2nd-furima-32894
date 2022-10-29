5d5d5c611d93854970cdec7981c213ffb2a9afbf
Emiri-Kakazu <emimail666@hotmail.co.jp>
2022-10-29 09:36:16 +0900

    index-items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -88,9 +88,8 @@ RSpec.describe Item, type: :model do
       it 'userとの紐付けができていないと保存されない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
-
     end
   end
 end
