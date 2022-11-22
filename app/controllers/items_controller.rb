4ec3301aac5fc9761a438059ddb3fbe48c854b27
Emiri-Kakazu <emimail666@hotmail.co.jp>
2022-11-22 14:52:00 +0900

    fix_delete_items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id != @item.user.id
+    if current_user.id == @item.user.id
       @item.destroy
     end
     redirect_to root_path
