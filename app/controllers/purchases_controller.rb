c5b7a0c895ae72e79b9e706b5b2c430fbab3b680
Emiri-Kakazu <emimail666@hotmail.co.jp>
2022-12-11 12:11:18 +0900

    form-object-implementation-2

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,9 +5,19 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    binding.pry
+    @purchase_item = PurchaseItem.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    if @purchase_item.valid?
+      @purchase_item.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
-
+  private
+  def purchase_params
+    params.require(:purchase_item).permit(:postal_code, :prefecture_id, :municipalities, :street_number, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 
 end
