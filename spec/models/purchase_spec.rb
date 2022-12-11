7b0b6a327de68de3793f83ede3cd0072e7155cab
Emiri-Kakazu <emimail666@hotmail.co.jp>
2022-12-11 16:14:27 +0900

    UT_Purchase_model

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase = FactoryBot.build(:purchase)
+  end
+  describe '商品購入機能' do
+    context '購入できる場合' do
+      it '商品idとユーザーidが存在して保存できる場合' do
+        expect(@purchase).to be_valid
+      end
+    end
+
+    context '購入できない場合' do
+      it '商品idが紐づいていないので保存できない' do
+        @purchase.item = nil
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Item must exist")
+      end
+
+      it 'ユーザーidが紐づいていないので保存できない' do
+        @purchase.user = nil
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
