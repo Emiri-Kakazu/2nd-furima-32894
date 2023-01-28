59afc2de83e3624f5a159382f6ef6c0aedc445f9
Emiri-Kakazu <emimail666@hotmail.co.jp>
2023-01-28 18:48:46 +0900

    destroy-needless-files

--- app/models/purchased_item.rb
+++
@@ -1,3 +0,0 @@
-class PurchasedItem < ApplicationRecord
-  belongs_to :item
-end
--- db/migrate/20221225144354_create_purchased_items.rb
+++
@@ -1,8 +0,0 @@
-class CreatePurchasedItems < ActiveRecord::Migration[6.0]
-  def change
-    create_table :purchased_items do |t|
-      t.references :item, foreign_key: true
-      t.timestamps
-    end
-  end
-end
